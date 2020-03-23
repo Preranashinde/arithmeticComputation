34fd06544a2afade3bba0488a28ae6eaa077b20c
Preranashinde <preranapshinde3009@gmail.com>
2020-03-23 12:25:30 -0300

    Store result in dictionary and read the values from dictionary to array

--- arithmetic.sh
+++ arithmetic.sh
@@ -6,24 +6,36 @@ read -p "Enter the second number:" b
 read -p "Enter the third number:" c
 
 #Declaring dictionary
-declare -A dict
+declare -A dictionary
+declare -B array
 
 #Calculate the results of arithmetic operation
 
-function calculate()
+function calculateTheResult()
 {
-	dict[operation1]=$((a + b * c))
-	dict[operation2]=$((a * b + c))
-	dict[operation3]=$((c + a / b))
-	dict[operation4]=$((a % b + c))
+	dictionary[operation1]=$((a + b * c))
+	dictionary[operation2]=$((a * b + c))
+	dictionary[operation3]=$((c + a / b))
+	dictionary[operation4]=$((a % b + c))
 }
 
-function print()
+#To print the result of all computations
+function toPrintResult()
 {
-	echo "$a + $b * $c = ${dict[operation1]}"
-	echo "$a * $b + $c = ${dict[operation2]}"
-	echo "$c + $a / $b = ${dict[operation3]}"
-	echo "$a % $b + $c = ${dict[operation4]}"
+	echo "$a + $b * $c = ${dictionary[operation1]}"
+	echo "$a * $b + $c = ${dictionary[operation2]}"
+	echo "$c + $a / $b = ${dictionary[operation3]}"
+	echo "$a % $b + $c = ${dictionary[operation4]}"
 }
-calculate
-print
+
+#Read the  values from dictionary into array
+function dictionaryToArray()
+{
+	for((index=0; index<4; index++))
+	do
+		array[index]=${dictionary[operation$((index+1))]}
+	done
+}
+calculateTheResult
+toPrintResult
+dictionaryToArray
