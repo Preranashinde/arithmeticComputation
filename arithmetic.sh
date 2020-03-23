73051a81af682ae4c9824ae519ab438bd9a3728d
Preranashinde <preranapshinde3009@gmail.com>
2020-03-23 11:35:01 -0300

    To compute first opeartion

--- arithmetic.sh
+++ arithmetic.sh
@@ -5,5 +5,19 @@ read -p "Enter the first number:" a
 read -p "Enter the second number:" b
 read -p "Enter the third number:" c
 
+#Declaring dictionary
+declare -A dict
 
+#Calculate the results of arithmetic operation
 
+function calculate()
+{
+	dict[operation1]=$((a + b * c))
+}
+
+function print()
+{
+	echo "$a + $b + $c = ${dict[operation1]}"
+}
+calculate
+print
